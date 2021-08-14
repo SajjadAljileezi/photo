6fc2633747c6f346f2ac4985938b6e4865248970
Sajjad Aljileezi <30444995+SajjadAljileezi@users.noreply.github.com>
2021-08-13 19:28:50 -0700

    first commit

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,397 @@
+<template>
+<div id="app">
+<section id="home" class=" headerv dd" >
+  <div class="overlay-wcs "></div>
+  <video playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop">
+    <source src="img/portland.mp4" type="video/mp4">
+  </video>
+  <div class="container h-100">
+    <div class="d-flex h-100 text-center align-items-center">
+      <div class="w-100 text-white">
+          <h1 class="mb-5 videot">For Real Estate Photography there are several solutions...but only one is right for your company.
+</h1>
+<span class="logos"><strong class="logog">MLS </strong>REAL ESTATE PHOTOGRAPHERS
+</span>
+
+      </div>
+    </div>
+  </div>
+
+</section>
+<div class="row bg-black ">
+    <div  id="about" class="  col-sm-12 text-center mt-5"> <h1 class=" head">About Us</h1> </div>
+</div>
+<div class="row bg-black ">
+    <div  id="about" class=" col-md-12 text-center mt-5"> <h1 class=" white">MLS Real Estate Photographer is Trusted Photographers in Real Estate Photography, Located in Portland, Offering 3D Virtual Reality, Aerial Drone Photography and video, Multifamily,Commercial Photography and Realtor Headshots!
+</h1> </div>
+</div>
+<div class="row bg-black ">
+    <div   id="about" class="  col-sm-3 text-center mt-5"> <font-awesome-icon icon="camera" size="5x" /><br> <span class="about">Experience</span><br><h1 class=" description">With more than 15 years in multimedia and 5 years experience in real estate photography, Portland Shutter photos and virtual tours help showcase your listing in its entirety.</h1></div>
+    <div   id="about" class="  col-sm-3 text-center mt-5"> <font-awesome-icon icon="unlock" size="5x" /> <br> <span class="about">No Limit</span> <br><h1 class=" description">We don’t limit our sessions to any certain number of photos. We will shoot whatever it takes to showcase the property properly. And you will have plenty of pictures to choose from.</h1></div>
+    <div   id="about" class="  col-sm-3 text-center mt-5"> <font-awesome-icon icon="images" size="5x" /> <br> <span class="about">Ready to Use</span><br><h1 class=" description">Easy to Deliver, Most photos are delivered Same Day or by midnight the evening of the shoot and Ready to Use For MLS, Website and Social Media.</h1> </div>
+    <div   id="about" class="  col-sm-3 text-center mt-5"> <font-awesome-icon icon="calendar-alt" size="5x" /> <br> <span class="about">Flexibility</span><br><h1 class=" description">We offer a range of flexible work arrangements to help our Clients achieve work/life balance, while meeting the demands of our business.</h1> </div>
+</div>
+<!-- Navs -->
+<div class="row bg-black">
+    <div class="container text-center mt-5">
+<ul class="nav nav-tabs" id="myTab" role="tablist">
+
+  <li class="nav-item col-sm-4">
+    <a class="nav-link active " id="what-tab" data-toggle="tab" href="#what" role="tab" aria-controls="what" aria-selected="true"><span class="navs">What</span></a>
+  </li>
+
+
+  <li class="nav-item col-sm-4">
+    <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false"><span class="navs">Why</span></a>
+  </li>
+  <li class="nav-item col-sm-4">
+    <a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false"><span class="navs">How</span></a>
+  </li>
+</ul>
+<div class="tab-content" id="myTabContent">
+  <div class="tab-pane fade show active" id="what" role="tabpanel" aria-labelledby="what-tab">
+      <div class="row">
+      <div  class="col-lg-6 mt-5"> <span class="scroll">  Architecture / Real Estate HD Photography – Video – Websites – Virtual Tours</span><br>
+ <h1 class=" description">Portland Shutter is dedicated to helping Realtors market their listings. We use the highest quality professional full frame Nikon cameras and lenses with supported speedlights, to capture the highest quality photographic images in the industry. Portland Shutter also uses its proprietary image processing system to deliver your professional real estate photos with clarity and realism. In addition to providing superior HDR real estate photography.</h1></div>
+       <div class="col-lg-6"> <div data-aos="fade-right" data-aos-duration="3000"><img src="img/slide.png" class=" mt-5 scroll "> </div> <div data-aos="fade-down"  data-aos-duration="3000" ><img src="img/slide2.png" class=" secound"></div> <div data-aos="zoom-in" data-aos-duration="3000" ><img src="img/slide3.png" class="third"></div></div>
+       </div>
+  </div>
+  <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab"><div class="row mt-5">
+      <div   class="col-lg-4 ">
+      <div class="col-lg-12 d-flex"><span class="p-2 scrolls"> Zillow video walkthroughs </span>  <br> <span class="ml-auto p-2"><font-awesome-icon icon="home" size="4x" /></span>
+</div>
+<div class="col-lg-12 d-flex"><p class="par">Create a video walkthrough for your home with Zillow. Give buyers an inside look and showcase your home's unique features.
+
+</p> </div>
+      <div class="col-lg-12"> <div class="col-lg-12 d-flex"><span class="p-2 scrolls"> Video virtual tours </span>  <br> <span class="ml-auto p-2"><font-awesome-icon icon="video" size="4x" /></span>
+</div>
+<div class="col-lg-12 d-flex"><p class="par">Whether you're in retail or real estate, a virtual tour can be a great way to give customers an inside look at what you have to offer.
+
+</p> </div></div>
+      <div class="col-lg-12"> <div class="col-lg-12 d-flex"><span class="p-2 scrolls"> Aerial drone photography and videos
+</span>  <br> <span class="ml-auto p-2"><font-awesome-icon icon="file-video" size="4x" /></span>
+</div>
+<div class="col-lg-12 d-flex"><p class="par">Aerial drone photography and drone video captures some of the most stunning images out there, it can bring a creative ways to best showcase your listing.
+
+
+
+</p> </div></div>
+      </div>
+      <div  class="col-lg-4 "> <img src="img/sli.png" class=" sli mt-5 "> </div>
+       <div  class="col-lg-4 ">
+      <div class="col-lg-12 d-flex"><span class="p-2 scrolls"> Matterport 3D tours </span>  <br> <span class="ml-auto p-2"><font-awesome-icon icon="play-circle" size="4x" /></span>
+</div>
+<div class="col-lg-12 d-flex"><p class="par">3D Camera to scan 4K virtual tours of real estate, hotels, retail etc. Create 3D floorplans, 360 panoramas & photography. Now on Google Street view and VR.
+
+</p> </div>
+      <div class="col-lg-12"> <div class="col-lg-12 d-flex"><span class="p-2 scrolls"> High end Cameras</span>  <br> <span class="ml-auto p-2"><font-awesome-icon icon="camera-retro" size="4x" /></span>
+</div>
+<div class="col-lg-12 d-flex"><p class="par">Bring the Home to the Buyer, Anywhere in the World!
+
+</p> </div></div>
+      <div class="col-lg-12"> <div class="col-lg-12 d-flex"><span class="p-2 scrolls"> Retina Compatible
+
+</span>  <br> <span class="ml-auto p-2"><font-awesome-icon icon="desktop" size="4x" /></span>
+</div>
+<div class="col-lg-12 d-flex"><p class="par">Retina display is a term which was introduced by Apple to refer to the displays in their devices with a high resolution and pixel density per inch.
+
+
+
+
+
+</p> </div></div>
+      </div>
+       </div>
+       </div>
+
+  <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">
+
+         <div class="row">
+
+        <div class="col-lg-3 mt-5"><div >  <img src="img/23.png" class="bed"> </div> </div> <div class="col-lg-3 mt-5"><div  data-aos="fade-down"  data-aos-duration="2000" ><img src="img/21.png" class=" bath"> </div></div>
+             <div   class="col-lg-6 mt-5"> <span class="scroll">  User-Friendly Photos/Videos </span><br>
+ <h1 class=" description">It's never been easier or more affordable to have magazine quality Ultra HD images for your Portland listing. Our photographers capture each property's unique story with an unlimited number of images.
+
+<br><br>Good real estate photos generate online interest and motivate home buyers or renters to contact you.</h1>
+
+<h5 class="mt-5 text-left  ">Video virtual tours</h5>
+<div class="progress mb-3">
+  <div class="progress-bar bg-danger progress-bar-striped progress-bar-animated" role="progressbar" style="width: 100%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">100%</div>
+</div>
+<h5 class="text-left  ">Aerial drone photography and videos</h5>
+<div class="progress mb-3">
+  <div class="progress-bar bg-danger progress-bar-striped progress-bar-animated" role="progressbar" style="width: 100%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">100%</div>
+</div>
+<h5 class="text-left  ">Matterport 3D tours</h5>
+<div class="progress mb-3">
+  <div class="progress-bar bg-danger progress-bar-striped progress-bar-animated" role="progressbar" style="width: 100%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">100%</div>
+</div>
+<h5 class="text-left  ">Ultra HD images</h5>
+<div class="progress mb-3">
+  <div class="progress-bar bg-danger progress-bar-striped progress-bar-animated" role="progressbar" style="width: 100%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">100%</div>
+</div>
+</div>
+
+       </div>
+  </div>
+  </div>
+</div>
+</div>
+
+
+<div class="pricing-container bg-price ">
+    <div id="prices" class="col-sm-12 text-center mb-5 "> <h1 class=" head"> prices</h1> </div>
+    <div class="row">
+    <div id="prices" class=" col-md-12 text-center mb-5"> <h1 class=" white">We've already put together packages that save you time and money. From photos, full aerial tours and Marketing, we have a package that is right for your listing.
+
+</h1> </div>
+</div>
+  <div class="row">
+
+    <div    class="col-sm-3">
+      <div class="pricing-table">
+        <div class="tb-border"></div>
+        <div class="lr-border"></div>
+        <div class="pricing-inner">
+           <div class="plan-name">
+            <p><font-awesome-icon icon="camera-retro" size="4x" /></p>
+            <h1> Photos</h1>
+           </div>
+          <div class="plan-price-container">
+            <p class="plan-price">100<sup><small>$</small></sup></p>
+          </div>
+          <div class="plan-desc">
+             <p>Unlimited Photos</p>
+              <p>HDR/Edited/Enhanced</p>
+            <p>Delivered Same Day</p>
+            <p>For houses up to 2,000 Sq.Ft</p>
+            <p>MLS Optimized photos</p>
+          </div>
+          <button>Choose Plan</button>
+         </div>
+      </div>
+    </div>
+    <div     class="col-sm-3">
+      <div class="pricing-table">
+        <div class="tb-border"></div>
+        <div class="lr-border"></div>
+        <div class="pricing-inner">
+           <div class="plan-name">
+            <p><font-awesome-icon icon="video" size="4x" /></p>
+            <h1>Aerial</h1>
+           </div>
+          <div class="plan-price-container">
+            <p class="plan-price">200<sup><small>$</small></sup></p>
+          </div>
+          <div class="plan-desc">
+             <p>Real Estate & Aerial Property Tours</p>
+              <p>4K Ultra HD Cameras</p>
+            <p>Delivered Same Day</p>
+            <p>Full Frame Views</p>
+            <p>Embed your Company Logo</p>
+          </div>
+          <button>Choose Plan</button>
+         </div>
+      </div>
+    </div>
+    <div    class="col-sm-3">
+      <div class="pricing-table">
+        <div class="tb-border"></div>
+        <div class="lr-border"></div>
+        <div class="pricing-inner">
+           <div class="plan-name">
+            <p><font-awesome-icon icon="file-video" size="4x" /></p>
+            <h1>3D Matterport</h1>
+           </div>
+          <div class="plan-price-container">
+            <p class="plan-price">250<sup><small>$</small></sup></p>
+          </div>
+          <div class="plan-desc">
+             <p>Compatible for Desktop or Mobile </p>
+              <p>Delivered Same Day</p>
+            <p>Free Hosting</p>
+            <p>Dollhouse View</p>
+            <p>For houses up to 3,000 sq ft</p>
+          </div>
+          <button>Choose Plan</button>
+         </div>
+      </div>
+    </div>
+    <div  class="col-sm-3">
+      <div class="pricing-table">
+        <div class="tb-border"></div>
+        <div class="lr-border"></div>
+        <div class="pricing-inner">
+           <div class="plan-name">
+            <p><font-awesome-icon icon="rocket" size="4x" /></p>
+            <h1>ADD-ON</h1>
+           </div>
+          <div class="plan-price-container">
+            <p class="plan-price">50<sup><small>$</small></sup></p>
+          </div>
+          <div class="plan-desc">
+             <p>Facebook Listing Page </p>
+              <p>Website For Listing </p>
+            <p>Manage Paid Ads For Website & Facebook</p>
+            <p>Upload Your Listing to Zillow & Trulia</p>
+          </div>
+          <button>Choose Plan</button>
+         </div>
+      </div>
+    </div>
+  </div>
+
+</div>
+<!--    Templates-->
+    <div class="templates-container bg-price ">
+        <div id="templates" class="col-sm-12 text-center mb-5 "> <h1 class=" head"> Templates</h1> </div>
+        <div class="row">
+            <div id="templates" class=" col-md-12 text-center mb-5"> <h1 class=" white">We've already put together packages that save you time and money. From photos, full aerial tours and Marketing, we have a package that is right for your listing.
+
+            </h1> </div>
+        </div>
+        </div>
+<!--    Templates-->
+
+
+<!-- Reviews -->
+<div class=" row bg-black ">
+
+    <div  id="testemonials" class="  col-sm-12 text-center mt-5"> <h1 class=" head">Testemonials</h1> </div>
+    <div class="row">
+    <div id="prices" class=" col-md-12 text-center "> <h1 class=" white">We are very fortunate to have formed excellent partnerships with many of our clients. And we’ve formed more than just working relationships with them; we have formed true friendships. Here’s what they’re saying about us.
+
+
+
+</h1> </div></div>
+
+    <div class="row">
+
+        <div class=" col-md-offset-2 col-md-6">
+            <div id="testimonial-slider" class="owl-carousel">
+
+                <div  class="testimonial">
+                    <div class="pic">
+                        <img src="img/kw.jpg" alt="" class="img-responsive">
+                    </div>
+                    <h3 id="testimonial-info" class="testimonial-info">
+                        Melissa Rodrigues
+                        <small> KW </small>
+                    </h3>
+                    <p class="description"> <font-awesome-icon icon="quote-left" size="1x" />   Awesome photos! Nice wide angles and lighting. Professional service - responsive, flexible, with a fast turnaround. Highly recommend!
+.<font-awesome-icon icon="quote-right" size="1x" /></p>
+                </div>
+            </div>
+
+    </div>
+      <div class=" col-md-offset-2 col-md-6">
+            <div id="testimonial-slider" class="owl-carousel">
+
+                <div  class="testimonial">
+                    <div class="pic">
+                        <img src="img/c21.jpg" alt="" class="img-responsive">
+                    </div>
+                    <h3 id="testimonial-info" class="testimonial-info">
+                        Julie Jedlanek
+                        <small>Summer Creek Realty</small>
+                    </h3>
+                    <p class="description"> <font-awesome-icon icon="quote-left" size="1x" /> Easy to book and reasonably priced pro RE shots. They also took the extra effort to shoot photos of the park across the street which was much appreciated.
+ <font-awesome-icon icon="quote-right" size="1x" /> </p>
+                </div>
+            </div>
+
+    </div>
+    </div>
+
+</div>
+<!-- Contact Us -->
+<div id="contacts"  class=" bg-price">
+    <div id="contact" class="col-sm-12 text-center mb-5 pt-5 "> <h1 class=" head"> Contact Us</h1> </div>
+    <div id="contact" class=" col-md-12 text-center mb-5"> <h1 class=" white">We’re here to help and answer any question you might have. We look forward to hearing from you 🙂
+
+
+
+</h1> </div>
+<hr>
+ <div   class="row">
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2795.6780255699464!2d-122.68162168469948!3d45.51655973796957!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5495a3d50dfb0577%3A0x58c2031962a8196f!2sReal+Estate+Photography!5e0!3m2!1sen!2sus!4v1558995822541!5m2!1sen!2sus" width="100%" height="320" frameborder="0" style="border:0" allowfullscreen></iframe> </div>
+	<div  class="row text-center">
+	  <div class="d-none d-sm-block col-4 box1 pt-4">
+        <a href="tel:(503) 544-3120"><i class="fas fa-phone fa-3x"></i>
+        <font-awesome-icon icon="phone-volume" size="2x" /> <h3 class="">Phone</h3>
+        <p class="">(503) 544-3120</p></a>
+      </div>
+      <div class=" d-none d-sm-block col-4 box2 pt-4">
+        <a href=""><i class="fas fa-home fa-3x"></i>
+        <font-awesome-icon icon="map-marker-alt" size="2x" /><h3 class="">Address</h3>
+        <p class="">1001 SW 5th Ave #1100, Portland, OR 97204</p></a>
+      </div>
+      <div class="col-4 box3 d-none d-sm-block pt-4">
+        <a href="mailto:customerservice@portlandshutter.com"> <font-awesome-icon icon="envelope-open" size="2x" />
+        <h3 class="">E-mail</h3>
+        <p class="">customerservice@portlandshutter.com</p></a>
+      </div>
+
+     <div class="  d-block d-sm-none col-12 box4 pt-4">
+        <a href="tel:(503) 544-3120">
+         <font-awesome-icon icon="phone-volume" size="2x" /> <h3 class="">Phone</h3>
+        <p class="phones">(503) 544-3120</p></a>
+      </div>
+      </div>
+</div>
+
+
+</div>
+</template>
+
+<script>
+
+
+
+  export default {
+    data() {
+      return {
+       width:1400,
+       height:400,
+        items: [{
+          src: 'img/1.jpg',
+          thumbnail: 'img/1t.jpg',
+          caption: ' MLS Real Estate Photographers'
+        },
+        {
+          src: 'img/2.jpg',
+          thumbnail: 'img/2t.jpg',
+          caption: ' MLS Real Estate Photographers'
+        },
+        {
+          src: 'img/3.jpg',
+          thumbnail: 'img/3t.jpg',
+          caption: ' MLS Real Estate Photographers'
+        },
+        {
+          src: 'img/4.jpg',
+          thumbnail: 'img/4t.jpg',
+
+caption: ' MLS Real Estate Photographers'},
+        {
+          src: 'img/5.jpg',
+          thumbnail: 'img/5t.jpg',
+
+caption: ' MLS Real Estate Photographers'},
+        {
+          src: 'img/6.jpg',
+          thumbnail: 'img/6t.jpg',
+
+caption: ' MLS Real Estate Photographers'},
+
+      ]};
+    },
+    components: {
+
+    }
+  }
+
+</script>
+
+
+
---
+++ resources/js/components/Templates.vue
@@ -0,0 +1,13 @@
+<template>
+<h1> this is templates</h1>
+</template>
+
+<script>
+export default {
+    name: "Templates"
+}
+</script>
+
+<style scoped>
+
+</style>
